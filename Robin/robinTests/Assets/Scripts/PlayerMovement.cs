7c651267105b276008a5789d5bf7f94a990cdc6d
gwnrobin <robinschellingerhout@live.nl>
2017-11-28 16:28:43 +0100

    eigenTest

---
+++ Robin/robinTests/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private int moveSpeed;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+    }
+}
---
+++ Robin/robinTests/Assets/Scripts/camera_focus.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_focus : MonoBehaviour {
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityY = 15F;
+    public float sensitivityX = 15F;
+    float rotationY = 0F;
+
+    void Update()
+    {
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse Y") * sensitivityY, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+    }
+}
