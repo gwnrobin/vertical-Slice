90fa5dada2c43643e57cd4e5085bfb94d36cfc95
Max Dekker <xamrekked@gmail.com>
2017-12-07 11:46:37 +0100

    Checkpoints, no UI
    Checkpoints zonder UI

---
+++ Max/StarWars - Max version/Assets/Scripts/UI/CheckPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+    public float currentCheckpoint = 10;
+    public float checkpointsMIN = 0;
+    public float checkpointsMAX = 12;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "Player")
+        {
+            currentCheckpoint += 1;
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		print(currentCheckpoint);
+	}
+}
