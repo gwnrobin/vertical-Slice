c3c2f2b36a5ff69ca62932a83a897abf04cf0c95
Feaxxae <duncan.westerdijk@quicknet.nl>
2017-12-21 10:59:06 +0100

    Everything
    The entire updated UI

---
+++ Duncan/StarwarsPhysicsTest/Assets/Scripts/Player_ActiveShield.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Player_ActiveShield : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _playerObject;
+
+    private float ShieldBar;
+
+    private bool ShieldActive = false;
+
+    [SerializeField]
+    private Image Bar;
+    [SerializeField]
+    private Image Charge;
+
+    float BarMin = 0.0f;
+    float BarMax = 1.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        Bar.fillAmount = BarMin;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ShieldActive == false)
+        {
+            if (Input.GetButton("Shield"))
+            {
+                Bar.fillAmount = BarMax;
+                ShieldActive = true;
+            }
+        }
+        if (Bar.fillAmount == BarMin)
+        {
+            Bar.fillAmount = BarMin;
+            ShieldActive = false;
+        }
+        Bar.fillAmount -= 0.001f;
+        Charge.fillAmount = Bar.fillAmount;
+	}
+}
---
+++ Duncan/StarwarsPhysicsTest/Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    [SerializeField]
+    private Text timerText;
+    int timerTick = 0;
+    float startTime;
+
+	// Use this for initialization
+	void Start () {
+        startTime = 600;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float time = startTime - Time.time;
+
+        string timeMinutes = ((int)time / 60).ToString();
+        string timeSeconds = ((int)time % 60).ToString();
+
+        timerText.text = timeMinutes + ":" + timeSeconds;
+    }
+}
