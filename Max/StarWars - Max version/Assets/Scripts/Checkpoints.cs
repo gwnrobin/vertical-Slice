760d06e7576701810a3a781f58069113f290ff34
MaxHDekker <xamrekked@gmail.com>
2017-12-22 20:34:35 +0100

    Add files via upload

---
+++ Max/StarWars - Max version/Assets/Scripts/Checkpoints.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoints : MonoBehaviour {
+
+    public float checkpoints = 12;
+    public float currentcheckpoint = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(currentcheckpoint);
+	}
+
+    void OnCollisionEnter(Collision col)
+    {   
+        if (col.collider.tag == "Checkpoint")
+        {
+            currentcheckpoint += 1;
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Max/StarWars - Max version/Assets/Scripts/ClampCheckpoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClampCheckpoint : MonoBehaviour {
+
+    public RawImage checkpoints;    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 checkPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        checkpoints.transform.position = checkPos;
+	}
+}
